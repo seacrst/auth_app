57b47d546328705e46cdb14c179c258addd33934
Seacrest <paganseacrest@gmail.com>
2024-05-19 16:16:29 +0300

    setup github action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("assets"))
+        .route("/hello", get(hello_handler));
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
